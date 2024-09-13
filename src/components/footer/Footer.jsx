e9d39eba03a7d9a47b2cb45df4cbbeafd8445571
YEAHx4 <developerstarlight@gmail.com>
2024-09-13 12:57:27 +0900

    Footer layout

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,17 @@
 import { FaGithub } from "react-icons/fa6";
+import cn from "../../../utils/cn";
 
 export default function Footer() {
   return (
     <footer className="flex justify-center">
-      <div className="flex justify-between max-w-[1024px] w-full">
+      <div
+        className={cn(
+          "flex justify-between max-w-[1024px] w-full items-center",
+          "border-t-2 border-gray-400 py-8 px-8"
+        )}
+      >
         <span>&copy; 2024 karpitony, YEAHx4.</span>
-        <FaGithub />
+        <FaGithub className="text-2xl" />
       </div>
     </footer>
   );
