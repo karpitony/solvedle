4c60f2e28d7dcc68622d206c6ee9fd33a3461bdb
Yunseok <watemark1017@gmail.com>
2024-09-21 13:27:25 +0900

    셀 크기 모바일에서 축소
    - 모바일에서 셀 크기 축소시킴
    - 태그 길이에 따라서 추가적으로 더 작게 만듬

--- src/components/game/Cell.jsx
+++ src/components/game/Cell.jsx
@@ -1,7 +1,25 @@
-import React, { useState } from "react";
 import cn from "../../lib/cn";
+import { useState, useEffect } from "react";
 
 function Cell({ tagLength, value }) {
+  const [cellSize, setCellSize] = useState("w-14 h-14");
+  
+  useEffect(() => {
+    if (tagLength === 7) {
+      setCellSize("w-12 h-12");
+    } else if (tagLength >= 8) {
+      setCellSize("w-10 h-10");
+    }
+  }, [tagLength]);
+
+  // userResult에 따라 동적으로 클래스 이름 결정
+  let baseClasses = cn(
+    `${cellSize} md:w-20 md:h-20`,
+    "border border-slate-500 rounded w-20 h-20 text-center",
+    "flex justify-center items-center select-none text-3xl",
+    "font-extrabold"
+  );
+
   return (
     <div className="flex gap-1">
       {value.map((letter, index) => (
@@ -10,9 +28,7 @@ function Cell({ tagLength, value }) {
           type="text"
           maxLength="1"
           className={cn(
-            "border border-slate-500 rounded w-20 h-20 text-center",
-            "flex justify-center items-center select-none text-3xl",
-            "font-extrabold"
+            baseClasses
           )}
         >
           {letter}
