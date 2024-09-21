1775d9ed97a110087559c4df808a8d2a6260b127
YEAHx4 <developerstarlight@gmail.com>
2024-09-21 14:15:05 +0900

    채점 및 배경색 변경

--- src/components/game/Cell.jsx
+++ src/components/game/Cell.jsx
@@ -1,17 +1,31 @@
+import { useEffect } from "react";
 import cn from "../../lib/cn";
 import { validateTag } from "../../lib/validator";
 
 function Cell({ tagLength, value, answer }) {
   const ans = validateTag(answer, value.join(""), tagLength);
 
+  useEffect(() => {
+    console.log(ans);
+  }, [ans]);
+
+  const colors = {
+    correct: "bg-green-200",
+    wrongLoc: "bg-blue-200",
+    jamoCorrect: "bg-yellow-200",
+    // wrongLocJamo: "bg-blue-200",
+    wrong: "bg-gray-200",
+  };
+
   return (
     <div className="flex gap-1">
-      {value.map((letter, index) => (
+      {value.map((letter, i) => (
         <div
-          key={index}
+          key={i}
           className={cn(
-            "w-24 h-24 border-2 border-slate-400 rounded font-extrabold",
-            "flex items-center justify-center select-none text-3xl"
+            "w-16 h-16 border-2 border-slate-400 rounded font-extrabold",
+            "flex items-center justify-center select-none text-3xl",
+            colors[ans[i]] || "bg-gray-200"
           )}
         >
           {letter}
--- src/lib/validator.js
+++ src/lib/validator.js
@@ -1,10 +1,57 @@
+import { jamo } from "./jamo";
+
 /**
  *
  * @param {string} answer
  * @param {string} tag
  * @param {number} length
- * @returns {Array<"empty" | "correct" | "wrongLoc" | >}
+ * @returns {Array<"correct" | "wrongLoc" | "jamoCorrect" | "wrongLocJamo"> | "empty"}
  */
 export const validateTag = (answer, tag, length) => {
   if (!tag) return "empty";
+
+  const result = [];
+  const ansJamo = jamo(answer);
+  const tagJamo = jamo(tag);
+
+  for (let i = 0; i < length; i++) {
+    if (answer[i] === tag[i]) {
+      result.push("correct");
+      continue;
+    }
+
+    if (answer.includes(tag[i])) {
+      result.push("wrongLoc");
+      continue;
+    }
+
+    let found = false;
+    for (let j of tagJamo[i]) {
+      if (ansJamo[i].includes(j)) {
+        result.push("jamoCorrect");
+        found = true;
+        break;
+      }
+    }
+
+    if (found) continue;
+
+    for (let j of tagJamo[i]) {
+      for (let k = 0; k < length; k++) {
+        if (k === i) continue;
+
+        if (ansJamo[k].includes(j)) {
+          result.push("wrongLocJamo");
+          found = true;
+          break;
+        }
+      }
+
+      if (found) break;
+    }
+
+    if (!found) result.push("wrong");
+  }
+
+  return result;
 };
