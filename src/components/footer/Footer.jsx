f2255a21cea1504ac90aaf954b77b4f3817b787e
YEAHx4 <developerstarlight@gmail.com>
2024-09-22 01:41:53 +0900

    저작권 표시

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,17 +4,22 @@ import cn from "../../lib/cn";
 export default function Footer() {
   return (
     <footer className="flex justify-center">
-      <div
-        className={cn(
-          "flex justify-between max-w-[1024px] w-full items-center",
-          "border-t-2 border-gray-400 py-8 px-8"
-        )}
-      >
-        <span>&copy; 2024 karpitony, YEAHx4.</span>
-        <div className="text-3xl">
-          <a href="https://github.com/karpitony/solvedle" target="_blank">
-            <FaGithub />
-          </a>
+      <div className="w-full max-w-[1024px] flex-col py-8 px-8">
+        <div
+          className={cn(
+            "flex justify-between max-w-[1024px] w-full items-center",
+            "border-t-2 border-gray-400 py-8"
+          )}
+        >
+          <span>&copy; 2024 karpitony, YEAHx4.</span>
+          <div className="text-3xl">
+            <a href="https://github.com/karpitony/solvedle" target="_blank">
+              <FaGithub />
+            </a>
+          </div>
+        </div>
+        <div>
+          This page is neither endorsed by nor associated with Solved.ac.
         </div>
       </div>
     </footer>
